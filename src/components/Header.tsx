7d4367a25c41a581a5e7bbd7a9505e838bd61e2d
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-01-14 19:58:23 +0200

    created components folder to hold all of  my components

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <header className="">
+
+        <nav className="">
+            <div className="">
+                <div className="">
+                    <h1 className="">Where is the world?</h1>
+                </div>
+                <div className="">
+                    <ul>
+                        <li className="">icon</li>
+                        <li className="">Dark Mode</li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
