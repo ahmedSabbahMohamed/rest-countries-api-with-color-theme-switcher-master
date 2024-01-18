994f9c73dc2eed8623cb6b7a2ba484b8eae1ac58
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-01-18 06:25:40 +0200

    created FilterByRegion to filter countries

---
+++ src/components/FilterByRegion.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+interface Props {
+  handleSelect: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+};
+
+function Filter( {handleSelect}: Props) {
+
+  const allRegions = ["Filter by Region", "Africa", "Americas", "Asia", "Europe", "Oceania"]
+
+  return (
+    <div className="p-5 w-full md:w-[250px]">
+
+      <div className="rounded-md shadow-xl md:w-[250px] bg-white dark:bg-d-dBlue">
+        <select
+          onChange={handleSelect}
+          className="outline-none border-none p-3 bg-transparent relative z-40 w-[98%] text-l-dGray dark:text-white"
+          defaultValue=""
+          name="region"
+          id="region"
+        >
+          {allRegions.map((region) => {
+            return (
+              <option key={region} value={region}>
+                {region}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default Filter
