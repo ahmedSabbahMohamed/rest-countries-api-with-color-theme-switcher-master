3879380cbd35f5d4a60591ed42dbdd7d2410a8db
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-01-18 06:28:59 +0200

    added main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className={darkMode ? "dark": ""}>
       <Header handleClick={() => setDarkMode(!darkMode)}/>
+      <Main />
     </div>
   );
 }
