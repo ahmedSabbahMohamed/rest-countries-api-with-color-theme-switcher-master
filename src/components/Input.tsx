face3c7ae62354e5360b071182a0f16c84be7d43
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-01-18 06:26:18 +0200

    created input component to search country

---
+++ src/components/Input.tsx
@@ -0,0 +1,27 @@
+import { IoIosSearch } from "react-icons/io";
+
+interface Props {
+  value: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function Input({value, handleChange}: Props) {
+  return (
+    <div className="relative p-5 md:p-0 w-full md:w-[420px]">
+      <label className="absolute left-9 md:left-3 top-1/2 -translate-y-1/2 text-gray-600" htmlFor="country">
+        <IoIosSearch />
+      </label>
+        <input
+            className="pl-9 outline-none rounded-md shadow-xl border-none p-3 w-full dark:bg-d-dBlue dark:text-white"
+            type="text"
+            name="country"
+            id="country"
+            placeholder="Search for a country..."
+            value={value}
+            onChange={handleChange}
+        />
+    </div>
+  );
+}
+
+export default Input
