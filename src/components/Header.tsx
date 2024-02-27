b2cbad2db6b49576b8f55795ee8fbefad4f3d67c
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-02-27 17:34:34 +0200

    move change theme logic from App to Header and added accessability

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import { BsMoonFill } from "react-icons/bs";
 
-interface HeaderProps {
-  handleClick: () => void; // Define the type for handleClick
-}
+function Header() {
 
-function Header({ handleClick }: HeaderProps) {
+  const themeToggler = () => {
+    var myApp = document.getElementById("app")
+    myApp?.classList.toggle("dark")
+  }
+
   return (
     <header className="bg-white dark:bg-d-dBlue p-5 shadow-xl">
       <nav className="container mx-auto">
@@ -13,16 +15,16 @@ function Header({ handleClick }: HeaderProps) {
             Where is the world?
           </h1>
 
-          <div className="">
-            <ul
-              onClick={handleClick}
+          <div>
+            <button
+              onClick={themeToggler}
               className="cursor-pointer flex items-center gap-2"
             >
-              <li className="dark:text-white">
+              <span className="dark:text-white">
                 <BsMoonFill />
-              </li>
-              <li className="">Dark Mode</li>
-            </ul>
+              </span>
+              <span className="">Dark Mode</span>
+            </button>
           </div>
         </div>
       </nav>
